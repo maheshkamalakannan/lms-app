e3f86a979c84a27167e7f0896b7e98c55a42b9ab
maheshkamalakannan <mahesh.k@madronesoft.com>
2013-07-30 16:50:57 +0530

    More test cases for DepartmentService and RoleService

--- src/test/java/com/madrone/lms/service/RoleServiceTest.java
+++ src/test/java/com/madrone/lms/service/RoleServiceTest.java
@@ -14,21 +14,21 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.madrone.lms.entity.Department;
 import com.madrone.lms.entity.DesignationEnum;
 import com.madrone.lms.entity.Employee;
+import com.madrone.lms.entity.Role;
 import com.madrone.lms.service.util.ServiceTestUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/applicationContext.xml", 
 "classpath:spring/hibernateContext.xml"})
-public class DepartmentServiceTest {
+public class RoleServiceTest {
 
 	@Autowired
 	private EmployeeService employeeService;
 
 	@Autowired
-	private DepartmentService departmentService;
+	private RoleService roleService;
 
 	private final String ROLE_R1 = "r1";
 	private final String DEPT_D1 = "d1";
@@ -38,99 +38,100 @@ public class DepartmentServiceTest {
 	@After
 	public void tearDown() throws Exception {
 		ServiceTestUtil.deleteEmployee(EMP_100);
-		ServiceTestUtil.deleteDepartment(DEPT_D1);
+		ServiceTestUtil.deleteRole(ROLE_R1);
 	}
 	
 	@Test
-	public void testCreateDepartment() throws Exception {
+	public void testCreateRole() throws Exception {
 		/*
-		 * Tests non-cascade Employee on creating Department. When updating
-		 * department, employee record should not be updated
+		 * Tests non-cascade Employee on creating Role. When updating
+		 * role, employee record should not be updated
 		 */
 		// delete records of last run if exist
 		ServiceTestUtil.deleteEmployee(EMP_100);
-		ServiceTestUtil.deleteDepartment(DEPT_D1);
+		ServiceTestUtil.deleteRole(ROLE_R1);
 		
-		Department d = new Department(DEPT_D1, "department-1");
+		Role r = new Role(ROLE_R1, "role-1");
 		Employee e = new Employee(EMP_100, "tom", "jerry", USER_NAME, null, 
 				Calendar.getInstance(), DesignationEnum.SSE, 
 				"#25 Chitrakulam north st", "Mylapore", "Chennai", 
 				"TN", 600004);
 		Set<Employee> eSet = new HashSet<Employee>();
 		eSet.add(e);
-		d.setEmployees(eSet);
+		r.setEmployees(eSet);
 
-		departmentService.saveDepartment(d);
+		roleService.saveRole(r);
 		
-		d = departmentService.findByIdWithEmployees(d.getId());
-		assertNotNull(d);
-		assertTrue(d.getEmployees().size() == 0);
+		r = roleService.findByIdWithEmployees(r.getId());
+		assertNotNull(r);
+		assertTrue(r.getEmployees().size() == 0);
 		
 		e = employeeService.findById(e.getId());
 		assertNull(e);		
 	}
 	
 	@Test
-	public void testUpdateDepartment() throws Exception {
+	public void testUpdateRole() throws Exception {
 		/*
-		 * Tests non-cascade Employee on updating Department. When updating
-		 * department, employee record should not be updated
+		 * Tests non-cascade Employee on updating Role. When updating
+		 * role, employee record should not be updated
 		 */
-		Department d = createDepartment();
+		Role r = createRole();
 		Employee e = createEmployee();
 		assertEquals(DesignationEnum.SSE, e.getDesignation());
 		
-		d = departmentService.findByIdWithEmployees(DEPT_D1);
-		assertTrue(d.getEmployees().contains(e));
+		r = roleService.findByIdWithEmployees(ROLE_R1);
+		assertTrue(r.getEmployees().contains(e));
 		
-		assertEquals("dept_1", d.getDescription());
-		d.setDescription("dept_1_updated");
+		assertEquals("role_1", r.getDescription());
+		r.setDescription("role_1_updated");
 		
 		e.setDesignation(DesignationEnum.TL);
 		
 		Set<Employee> eSet = new HashSet<Employee>();
 		eSet.add(e);
-		d.setEmployees(eSet);
+		r.setEmployees(eSet);
 		
-		departmentService.saveDepartment(d);
+		roleService.saveRole(r);
 		
-		d = departmentService.findByIdWithEmployees(DEPT_D1);
-		assertEquals("dept_1_updated", d.getDescription());
+		r = roleService.findByIdWithEmployees(ROLE_R1);
+		assertEquals("role_1_updated", r.getDescription());
 		
 		e = employeeService.findById(EMP_100);
 		assertEquals(DesignationEnum.SSE, e.getDesignation());		
 	}
 	
+
 	@Test
 	public void testDeleteEmployee() throws Exception {
-		Department d = createDepartment();
+		Role r = createRole();
 		createEmployee();
 		Employee e = employeeService.findById(EMP_100);
 		assertNotNull(e);
-		assertEquals(d, e.getDepartment());
+		assertEquals(r, e.getRole());
 		employeeService.deleteEmployee(EMP_100);
 		assertNull(employeeService.findById(EMP_100));
 		
-		// Department should be deleted when Employee is deleted
-		assertNotNull(departmentService.findById(e.getDepartment().getId()));	
+		// Role should be deleted when Employee is deleted
+		assertNotNull(roleService.findById(e.getRole().getId()));	
 	}	
 
 	@Test
-	public void testDeleteDepartment() throws Exception {
-		Department d = createDepartment();
+	public void testDeleteRole() throws Exception {
+		Role r = createRole();
 		createEmployee();
 		Employee e = employeeService.findById(EMP_100);
 		assertNotNull(e);
-		assertEquals(d, e.getDepartment());
+		assertEquals(r, e.getRole());
 		
 		try {
-			departmentService.deleteDepartment(d.getId());
+			roleService.deleteRole(r.getId());
 			fail("Should not reach here...");
 		} catch(DataIntegrityViolationException ex) {			
 		}
 				
 		employeeService.deleteEmployee(e.getId());
-		departmentService.deleteDepartment(d.getId());
+		roleService.deleteRole(r.getId());
 	}
 	
 	private Employee createEmployee() {
@@ -138,8 +139,8 @@ public class DepartmentServiceTest {
 				ROLE_R1, USER_NAME);
 	}
 	
-	private Department createDepartment() {
-		return ServiceTestUtil.createDepartment(DEPT_D1, "dept_1");		
+	private Role createRole() {
+		return ServiceTestUtil.createRole(ROLE_R1, "role_1");		
 	}
 
 }
