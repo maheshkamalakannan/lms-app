46de7733c5c7c6b78bf29dd697947303cea7ba33
senthilksp <senthilkumar.sp@madronesoft.com>
2013-08-08 14:52:07 +0530

    Constant File changes are checked in.

--- src/main/java/com/madrone/lms/constants/LMSConstants.java
+++ src/main/java/com/madrone/lms/constants/LMSConstants.java
@@ -2,16 +2,17 @@ package com.madrone.lms.constants;
 
 public class LMSConstants {
 	
-	public static final String ADMIN_LEVEL = "ADMIN";
-	public static final String SUPERVISOR_LEVEL = "SUPERVISOR";
-	public static final String MANAGER_LEVEL = "MANAGER";
-	public static final String EMPLOYEE_LEVEL = "EMPLOYEEE";
-	
-	public static final String ADMIN_MENU    	= "adminMenu";
-	public static final String MANAGER_MENU 	= "managerMenu";
-	public static final String EMPLOYEE_MENU 	= "employeeMenu";
-	public static final String SUPERVISOR_MENU 	= "supervisorMenu";
-	public static final String LOGIN_MENU 		= "login";
-	
+	public static final String ADMIN_LEVEL 			= "ADMIN";
+	public static final String SUPERVISOR_LEVEL 	= "SUPERVISOR";
+	public static final String MANAGER_LEVEL 		= "MANAGER";
+	public static final String EMPLOYEE_LEVEL 		= "EMPLOYEEE";
 
+	
+	public static final String LOGIN_MENU 			= "login";
+	
+	public static final String CHANGE_PASSWORD_SCR 	=  "changePasswordScreen";
+	public static final String EMPLOYEE_HOME_SCR    =  "employeeHomeScreen";
+	public static final String APPLY_LEAVE_SCR 		=  "applyLeaveScreen";
+	public static final String CANCEL_LEAVE_SCR 	=  "cancelLeaveScreen";
+	public static final String LEAVE_SUMMARY_SCR 	=  "leaveSummaryScreen";
 }
