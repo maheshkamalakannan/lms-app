5064d3947b81d5bf31083b54b0349e95304c1d67
senthilksp <senthilkumar.sp@madronesoft.com>
2013-08-20 23:34:27 +0530

    Java Bean [Forms] Files and images are checked in.

--- src/main/java/com/madrone/lms/form/ApplyLeaveForm.java
+++ src/main/java/com/madrone/lms/form/ApplyLeaveForm.java
@@ -1,40 +1,100 @@
 package com.madrone.lms.form;
 
+import com.madrone.lms.entity.LeaveStatusEnum;
+
 public class ApplyLeaveForm {
 	
-	private String userName;
-	private String empName;
+	private String empId;
 	
+	private String fromDate;
+	private String toDate;
+	private String fromDateSession;
+	private String toDateSession;
+	
+	private String leaveType;
+	private int noOfDays;
+	private String reason;
+	private String emergencyPhone;
+	private LeaveStatusEnum status;
+	
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public LeaveStatusEnum getStatus() {
+		return status;
+	}
+	public void setStatus(LeaveStatusEnum status) {
+		this.status = status;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getLeaveType() {
+		return leaveType;
+	}
+	public String getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+	public String getToDate() {
+		return toDate;
+	}
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
+	}
+	public int getNoOfDays() {
+		return noOfDays;
+	}
+	public void setNoOfDays(int noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getEmergencyPhone() {
+		return emergencyPhone;
+	}
+	public void setEmergencyPhone(String emergencyPhone) {
+		this.emergencyPhone = emergencyPhone;
+	}
+	
+	public String getFromDateSession() {
+		return fromDateSession;
+	}
+	public void setFromDateSession(String fromDateSession) {
+		this.fromDateSession = fromDateSession;
+	}
+	public String getToDateSession() {
+		return toDateSession;
+	}
+	public void setToDateSession(String toDateSession) {
+		this.toDateSession = toDateSession;
+	}
 
-	public String getUserName() {
-		return userName;
-	}
-	
-	
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	
 	@Override
 	public String toString() {
-		return "ApplyLeaveForm [userName=" + userName + ", empName=" + empName
-				+ "]";
+		return "ApplyLeaveForm [empId=" + empId + "]";
 	}
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((empName == null) ? 0 : empName.hashCode());
-		result = prime * result
-				+ ((userName == null) ? 0 : userName.hashCode());
+		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
 		return result;
 	}
+	
+	
+	
+	
 	
 }
